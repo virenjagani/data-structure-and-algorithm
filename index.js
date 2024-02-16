b2e9e60379b0b8fac6aa605c020d7bf62474fb17
virenjagani <virenjagani0303@gmail.com>
2024-02-16 12:09:30 -0500

    Find a peak element which is not smaller than its neighbours

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-//Find the minimum and maximum element in an array without SORT method.
+// Find a peak element which is not smaller than its neighbours
 console.clear();
 
 const element = [1, 3, 5, 2, 20, 54, 23, 3, 67, 68, 69, 30];
 
-var min = element[0]
-var max = element[0]
+const result = [];
 
-element.forEach((item,index,arr)=>{
-    if(arr[index]<min) min=item;
-    if(arr[index]>max) max=item;
-})
+element.forEach((item, index, arr) => {
+  if (index > 0) {
+    if (arr[index] >= arr[index + 1] && arr[index] >= arr[index - 1]) {
+      result.push(item);
+    }
+  } else if (index === 0) {
+    if (arr[0] > arr[1]) {
+      result.push(arr[0]);
+    }
+  }
+});
 
-console.log("minimum: ", min, " && maximum: ", max);
+console.log(result);
