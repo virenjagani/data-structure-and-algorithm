2167c6716d47aa42308a8dc21f136888334a21db
virenjagani <virenjagani0303@gmail.com>
2024-02-16 12:07:47 -0500

    Find the minimum and maximum element in an array without SORT method

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//Write a program to sort the array without SORT method
+//Find the minimum and maximum element in an array without SORT method.
 console.clear();
 
 const element = [1, 3, 5, 2, 20, 54, 23, 3, 67, 68, 69, 30];
 
-let flag = false
+var min = element[0]
+var max = element[0]
 
-do{
-   
-    
-}while(flag)
+element.forEach((item,index,arr)=>{
+    if(arr[index]<min) min=item;
+    if(arr[index]>max) max=item;
+})
 
-console.log(element);
+console.log("minimum: ", min, " && maximum: ", max);
