8157257cb1ec6c578d483f41f417ed133e861655
virenjagani <virenjagani0303@gmail.com>
2024-02-16 12:04:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Write a program to sort the array without SORT method
+console.clear();
+
+const element = [1, 3, 5, 2, 20, 54, 23, 3, 67, 68, 69, 30];
+
+let flag = false
+
+do{
+   
+    
+}while(flag)
+
+console.log(element);
